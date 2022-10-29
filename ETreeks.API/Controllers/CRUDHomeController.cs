5d336787d069d8b793d12521e0d603137f64f98e
ABDALLAH BATAYNEH <ambatayneh197@cit.just.edu.jo>
2022-10-29 19:00:43 +0200

    Add Home

---
+++ ETreeks.API/Controllers/CRUDHomeController.cs
@@ -0,0 +1,16 @@
+﻿using ETreeks.CORE.Data;
+using ETreeks.CORE.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETreeks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRUDHomeController : CRUDController<HomePage>
+    {
+        public CRUDHomeController(IService<HomePage> Homeservice) : base(Homeservice)
+        {
+        }
+    }
+}
