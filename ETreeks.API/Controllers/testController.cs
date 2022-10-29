361d8f4eb44fffe37d43573d08748b31f2a57278
mohammed <mohammedsaadeh473@gmail.com>
2022-10-29 18:13:45 +0200

    test

---
+++ ETreeks.API/Controllers/testController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETreeks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+    }
+}
