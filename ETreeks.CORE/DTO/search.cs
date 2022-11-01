76b5f29a1f2ccb99b2cd23c423fe839f3cf91465
mohammed <mohammedsaadeh473@gmail.com>
2022-11-01 22:25:15 +0300

    search reservation

---
+++ ETreeks.CORE/DTO/search.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETreeks.CORE.DTO
+{
+    public class search
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public DateTime? Start_Date { get; set; }
+        public DateTime? End_Date { get; set; }
+        public decimal? Status { get; set; }
+        public string COURSE_NAME { get; set; }
+        public decimal? id { get; set; }
+
+    }
+}
