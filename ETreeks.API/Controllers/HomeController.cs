d2492d331dda78aa18bc2070eda58696e8737bba
Designer <Designer@DESKTOP-A7M9MSC>
2022-11-18 00:15:47 +0300

    home upload image

---
+++ ETreeks.API/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using ETreeks.CORE.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace ETreeks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [Route("uploadImage")]
+        [HttpPost]
+        public HomePage UploadIMage()
+        {
+            var file = Request.Form.Files[0];
+            var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var fullPath = Path.Combine(@"C:\ETreeks\src\assets\images", fileName);
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            HomePage item = new HomePage();
+            item.Image = fileName;
+            return item;
+
+        }
+    }
+}
