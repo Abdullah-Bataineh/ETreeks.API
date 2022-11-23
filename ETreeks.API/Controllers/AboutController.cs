db8042322acd279ad2e076a99e8fa606769a1c23
mohammed <mohammedsaadeh473@gmail.com>
2022-11-23 14:32:16 +0300

    about

---
+++ ETreeks.API/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using ETreeks.CORE.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace ETreeks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        [Route("uploadImage")]
+        [HttpPost]
+        public About UploadIMage()
+        {
+            var file = Request.Form.Files[0];
+            var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var fullPath = Path.Combine(@"C:\ETreeks\src\assets\images", fileName);
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            About item = new About();
+            item.Image = fileName;
+            return item;
+
+        }
+    }
+}
