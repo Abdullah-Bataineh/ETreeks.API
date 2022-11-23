26b34e89032aaa7065b6b0775bf47e50f6e1670e
mohammed <mohammedsaadeh473@gmail.com>
2022-11-23 14:22:34 +0300

    aa

---
+++ ETreeks.API/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using ETreeks.CORE.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace ETreeks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [Route("uploadImage")]
+        [HttpPost]
+        public Category UploadIMage()
+        {
+            var file = Request.Form.Files[0];
+            var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var fullPath = Path.Combine(@"C:\ETreeks\src\assets\images", fileName);
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            Category item = new Category();
+            item.IMAGE = fileName;
+            return item;
+
+        }
+    }
+}
