9c9145751a1ff5eeb2ff047937e0bf454b85d732
mohammed <mohammedsaadeh473@gmail.com>
2022-11-23 03:04:26 +0300

    id

--- ETreeks.CORE/DTO/CourseWithCategory.cs
+++ ETreeks.CORE/DTO/CourseWithCategory.cs
@@ -6,7 +6,8 @@ namespace ETreeks.CORE.DTO
 {
     public class CourseWithCategory
     {
-     
+        public decimal id { get; set; }
+        public decimal? Cat_Id { get; set; }
         public string COURSE_NAME { get; set; }
         public string Description { get; set; }
         public string CATEGORY_NAME { get; set; }
