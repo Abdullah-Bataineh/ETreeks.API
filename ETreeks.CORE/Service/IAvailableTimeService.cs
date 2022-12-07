81c9a5e6e422310941d3bd2f4a29ec86640067bd
mohammed <mohammedsaadeh473@gmail.com>
2022-12-08 00:10:50 +0300

    update

--- ETreeks.CORE/Service/IAvailableTimeService.cs
+++ ETreeks.CORE/Service/IAvailableTimeService.cs
@@ -8,5 +8,6 @@ namespace ETreeks.CORE.Service
     public interface IAvailableTimeService
     {
         List<AvailableTime> GetByTrainer(int trainerId);
+        public void updateStatusbyID(int id, int st);
     }
 }
